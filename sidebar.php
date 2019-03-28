f1b53dc35a1a5de6799ecd53958f3c2046d9bb7b
Wes Kempfer <wkempferjr@tnotw.com>
2019-03-27 19:54:42 -0700

    Disabled sidebar

---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package siteorigin-unwind
+ * @since siteorigin-unwind 0.1
+ * @license GPL 2.0
+ */
+
+return;
